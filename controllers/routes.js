be64b3ed7e8696e992740dfcc068050b0e72de97
Michael Osgood <michaelosgood@dhcp-v120-099.mobile.uci.edu>
2017-09-28 19:20:31 -0700

    commit

--- controllers/routes.js
+++ controllers/routes.js
@@ -18,7 +18,8 @@ router.put('/burgers/update', function(req,res) {
 
 router.post('/burgers/create', function(req,res) {
     burger.create(req.body.burger_name, function(result) {
-
+        console.log(result);
+        res.redirect('/');
     })
 })
 
